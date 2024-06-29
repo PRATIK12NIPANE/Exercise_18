d8b5a922b74ec8f9e9fca4f7295682a0760aca4b
PRATIK12NIPANE <136259872+PRATIK12NIPANE@users.noreply.github.com>
2024-06-29 09:34:53 +0530

    first commit

---
+++ MyFile.js
@@ -0,0 +1,8 @@
+const MyFile = () => {
+    return ( 
+
+        <h1>hello from my file</h1>
+     );
+}
+ 
+export default MyFile;
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import MyFile from "./MyFile";
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<MyFile />)
